0f0b6b6e7f068e713ee8d60a2a5b9d59e514e526
Johnny.Wang <johnny.wang@btse.com>
2025-03-22 16:56:50 +0800

    feat: Jump Game II DP approach done

--- leetcode/src/main.rs
+++ leetcode/src/main.rs
@@ -3,9 +3,12 @@ mod easy;
 mod medium;
 mod common;
 
+use core::num;
+
 use crate::common::Solution;
 
 fn main() {
     //let res = Solution::is_match(String::from("abbb"), String::from("a*b"));
     //println!("final answer: {}", res);
+    let res = Solution::jump(vec![2,3,1,1,4]);
 }
