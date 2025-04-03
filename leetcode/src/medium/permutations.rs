a89b0e3edc92b6a2fcccbe84c32ef43a1cde606c
Johnny.Wang <johnny.wang@btse.com>
2025-04-03 14:34:50 +0800

    feat: Permutations done

--- leetcode/src/medium/permutations.rs
+++ leetcode/src/medium/permutations.rs
@@ -2,9 +2,36 @@ use crate::common::Solution;
 
 
 impl Solution {
+
     pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        return vec![vec![]];
+
+        // create a resut vector.
+        let mut results: Vec<Vec<i32>> = vec![];
+        let mut nums = nums;
+
+        // call the backtracking function.
+        Self::backtracking(&mut results, &mut nums, 0);
+
+        return results;
     }
+
+    fn backtracking(results: &mut Vec<Vec<i32>>, nums: &mut Vec<i32>, idx: usize) {
+        
+
+        // if the idx is equal to the length of the nums, copy the vector to the results and return.
+        if idx == nums.len() {
+            results.push(nums.clone());
+            return;
+        }
+
+        // do swapping.
+        for i in idx..nums.len() {
+            nums.swap(idx, i);
+            Self::backtracking(results, nums, idx + 1);
+            nums.swap(idx, i);
+        }
+    }
+
 }
 
 // -----------------------------------------------------------------
@@ -16,7 +43,7 @@ mod tests {
 
     #[test]
     fn test_1() {
-        assert_eq!(Solution::permute(vec![1,2,3]), vec![vec![1,2,3], vec![1,3,2], vec![2,1,3], vec![2,3,1], vec![3,1,2], vec![3,2,1]]);
+        assert_eq!(Solution::permute(vec![1,2,3]), vec![vec![1,2,3], vec![1,3,2], vec![2,1,3], vec![2,3,1], vec![3,2,1], vec![3,1,2]]);
     }
 
     #[test]
--- note/medium/046_permutations.md
+++ note/medium/046_permutations.md
@@ -26,12 +26,47 @@ https://leetcode.com/problems/permutations/description/
 
 I remember I did this before in Golang implement [link](https://github.com/Johnny1110/GO_LeetCode_Journey/tree/main/bonus/permutation).
 
-just convert to rust language. let try this with backtracking approach.
+just convert to rust language. let try this with backtracking approach, and I think it is gonna be more difficult because of Rust's ownership stuff...
 
 <br>
 
 ### Backtracking
 
 ```rust
+impl Solution {
 
+    pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
+
+        // create a resut vector.
+        let mut results: Vec<Vec<i32>> = vec![];
+        let mut nums = nums;
+
+        // call the backtracking function.
+        Self::backtracking(&mut results, &mut nums, 0);
+
+        return results;
+    }
+
+    fn backtracking(results: &mut Vec<Vec<i32>>, nums: &mut Vec<i32>, idx: usize) {
+        
+
+        // if the idx is equal to the length of the nums, copy the vector to the results and return.
+        if idx == nums.len() {
+            results.push(nums.clone());
+            return;
+        }
+
+        // do swapping.
+        for i in idx..nums.len() {
+            nums.swap(idx, i);
+            Self::backtracking(results, nums, idx + 1);
+            nums.swap(idx, i);
+        }
+    }
+
+}
 ```
+
+<br>
+
+pretty ez.
