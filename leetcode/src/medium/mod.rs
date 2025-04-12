d9a146cbbf32c4364d36440f0e6e48b6b325d60c
Johnny.Wang <johnny.wang@btse.com>
2025-04-12 14:54:17 +0800

    feat: rotate image done

--- leetcode/src/medium/mod.rs
+++ leetcode/src/medium/mod.rs
@@ -1,3 +1,4 @@
 mod jump_game_2;
 mod permutations;
 mod permutations_II;
+mod rotate_image;
--- leetcode/src/medium/rotate_image.rs
+++ leetcode/src/medium/rotate_image.rs
@@ -4,7 +4,37 @@ use crate::common::Solution;
 
 impl Solution {
     pub fn rotate(matrix: &mut Vec<Vec<i32>>) {
-        // TODO: implement the rotate function.
+        let side_length = matrix.len() as usize;
+
+        // 1. do flip horizontally.
+        Self::flip_horizontally(matrix, side_length);
+        // 2. do flip diagonally.
+        Self::flip_diagonally(matrix, side_length);
+    }
+
+    // How to flip horizontally?
+    // Swap Matrix(row, col) to Matrix(n-1-row, col)
+    fn flip_horizontally(matrix: &mut Vec<Vec<i32>>, side_length: usize) {
+        for i in 0..side_length / 2 {
+            for j in 0..side_length {
+                let temp = matrix[i as usize][j as usize];
+                matrix[i as usize][j as usize] = matrix[side_length - 1 - i as usize][j as usize];
+                matrix[side_length - 1 - i as usize][j as usize] = temp;
+            }
+        }
+    }
+
+    // How to flip diagonally?
+    // Swap Matrix(row, col) to Matrix(col, row)
+    fn flip_diagonally(matrix: &mut Vec<Vec<i32>>, side_length: usize) {
+        for i in 0..side_length {
+            for j in i..side_length {
+                let temp = matrix[i as usize][j as usize];
+                matrix[i as usize][j as usize] = matrix[j as usize][i as usize];
+                matrix[j as usize][i as usize] = temp;
+            }
+        }
+
     }
 }
 
